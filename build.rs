876d97be6ab69639177d3c40a4449948b63de347
germän gômez <germangb42@gmail.com>
2018-01-31 03:51:00 +0100

    fix build

--- build.rs
+++ build.rs
@@ -1,23 +1,21 @@
 extern crate cc;
 extern crate bindgen;
 
+use std::fs;
+
 fn main() {
     let bindings = bindgen::Builder::default()
         .header("minimp3/minimp3.h")
-        //.layout_tests(false)
         .generate()
         .expect("Unable to generate minimp3 bindings");
 
-    bindings
-        .write_to_file("src/bindgen.rs")
-        .expect("Unable to write bindings to output file");
+    bindings.write_to_file("src/bindgen.rs").expect("Unable to write bindings to output file");
 
+    fs::copy("minimp3/minimp3.h", "minimp3/minimp3.c").expect("Can't copy minimp3 source file");
 
     cc::Build::new()
-        .file("minimp3/minimp3.h")
+        .file("minimp3/minimp3.c")
         .define("MINIMP3_IMPLEMENTATION", None)
         .define("MINIMP3_NO_WAV", None)
         .compile("minimp3");
-
-    //println!("cargo:rustc-link-lib=static=minimp3");
 }
