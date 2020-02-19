cfc7ec22b56af33cce5342d192a8e8916f4215b6
german gomez <germangb42@gmail.com>
2020-02-19 20:01:23 +0100

    bump version

--- src/error.rs
+++ src/error.rs
@@ -40,7 +40,7 @@ impl StdError for Error {
         }
     }
 
-    fn cause(&self) -> Option<&StdError> {
+    fn cause(&self) -> Option<&dyn StdError> {
         match self {
             Error::Io(io_err) => Some(io_err),
             _ => None,
